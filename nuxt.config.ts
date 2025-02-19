75348a58fa35a1aecdd43fcf19c0cb7beb498833
Artem Bondareko <artemzi1000@gmail.com>
2025-02-19 21:23:04 +0700

    Setup Nuxt project with UI, ESLint, and TypeScript configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  typescript: {
+    typeCheck: true
+  },
+
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  },
+  css: ['~/assets/css/main.css'],
 })
