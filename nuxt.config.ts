182f901b5fa313269c3b263d62b63dcfd0cecb67
Artem Bondareko <artemzi1000@gmail.com>
2025-02-20 01:06:25 +0700

    Add GitHub Pages deployment workflow, markdown rendering, and Google Docs API integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,24 @@
+// off eslint
+/* eslint-disable node/prefer-global/process */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
   typescript: {
-    typeCheck: true
+    typeCheck: true,
   },
 
   modules: ['@nuxt/eslint', '@nuxt/ui'],
   eslint: {
     config: {
-      stylistic: true
-    }
+      stylistic: true,
+    },
   },
   css: ['~/assets/css/main.css'],
+
+  runtimeConfig: {
+    GOOGLE_DOC_ID: process.env.GOOGLE_DOC_ID,
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+  },
 })
