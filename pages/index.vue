dad969be3e6c6407d6bb1de78b0e8661122db987
Artem Bondareko <artemzi1000@gmail.com>
2025-03-21 01:05:02 +0700

    Обновлен компонент card.vue с добавлением поддержки изображения и эффекта наведения, изменен шаблон в index.vue для использования нового компонента, удален устаревший файл doc.get.ts, исправлен tsconfig.json для корректного форматирования.

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,10 @@
 </script>
 
 <template>
-  <!-- <div class="flex justify-center items-center h-screen">
-    <UText>Hello</UText>
-  </div> -->
-  <div class="flex flex-col gap-4 h-screen overflow-y-auto w-full">
-    <UContainer v-for="item in 10" class="flex rounded-lg m-4 h-70 bg-radial from-zinc-800 to-zinc-900 to-70%" style="height: 370px; min-height: 370px;">
-      <UText>Hello</UText>
-    </UContainer>
+  <div class="flex justify-center items-center h-screen">
+    <Card>
+      Hello
+    </Card>
   </div>
 </template>
 
