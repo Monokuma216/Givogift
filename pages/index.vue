98f90771e6d5d11a65101878826cc0ee2ae75923
Artem Bondareko <artemzi1000@gmail.com>
2025-06-05 23:52:55 +0700

    restore Rating component

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,10 @@
 <script lang="ts" setup>
-// const rating = ref<1 | 2 | 3 | 4 | 5>(3);
+const rating = ref<1 | 2 | 3 | 4 | 5>(3);
 </script>
 
 <template>
   <div class="size-full flex items-center justify-center">
-    <!--        <Rating v-model="rating" class="mt-4" />-->
-    <div>
-
-    </div>
+    <Rating v-model="rating" class="mt-4"/>
   </div>
 </template>
 
